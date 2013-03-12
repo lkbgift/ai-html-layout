e313954467a8fcf3f54d3e8c8ce180dda59ea02f
lkbgift <lkbgift@gmail.com>
2013-03-12 09:41:30 -0400

    changes resolves

--- Gruntfile.js
+++ Gruntfile.js
@@ -155,25 +155,7 @@ module.exports = function (grunt) {
         /*concat: {
             dist: {}
         },*/
-        /*requirejs: {
-            dist: {
-                // Options: https://github.com/jrburke/r.js/blob/master/build/example.build.js
-                options: {
-                    // `name` and `out` is set by grunt-usemin
-                    baseUrl: 'app/scripts',
-                    optimize: 'none',
-                    // TODO: Figure out how to make sourcemaps work with grunt-usemin
-                    // https://github.com/yeoman/grunt-usemin/issues/30
-                    //generateSourceMaps: true,
-                    // required to support SourceMaps
-                    // http://requirejs.org/docs/errors.html#sourcemapcomments
-                    preserveLicenseComments: false,
-                    useStrict: true,
-                    wrap: true,
-                    //uglify2: {} // https://github.com/mishoo/UglifyJS2
-                }
-            }
-        },*/
+        
         useminPrepare: {
             html: '<%= yeoman.app %>/index.html',
             options: {
@@ -200,7 +182,7 @@ module.exports = function (grunt) {
         cssmin: {
             dist: {
                 files: {
-                    '<%= yeoman.dist %>/css/main.css': [
+                    '<%= yeoman.dist %>/css/app.css': [
                         '.tmp/css/{,*/}*.css',
                         '<%= yeoman.app %>/css/{,*/}*.css'
                     ]
