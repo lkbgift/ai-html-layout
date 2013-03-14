64b042a08cfd5155e4e9bb2af018a5dc7d692d4a
lkbgift <lkbgift@gmail.com>
2013-03-14 17:06:58 -0400

    removing magic numbers

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 /*global define */
 define([], function () {
     'use strict';
-
-    return '\'Allo \'Allo!';
+    $(document).foundation('topbar');
+    $('#featured').foundation('orbit', { timer_speed: 5000, stack_on_small: false, animation_speed: 500, timer: false, bullets: true, }, function (response) {});
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 require.config({
   paths: {
-    jquery: 'vendor/jquery'
+    jquery: '../components/jquery/jquery'
   },
   shim: {
     "foundation/foundation" : { deps: ["jquery"] },
@@ -35,7 +35,7 @@ require(["jquery",
   "foundation/foundation.section",    
   "foundation/foundation.tooltips",   
   "foundation/foundation.topbar"
-  ], function ($) {
+], function ($) {
   $(document).foundation('topbar');
   $('#featured').foundation('orbit', { timer_speed: 5000, stack_on_small: false, animation_speed: 500, timer: false, bullets: true, }, function (response) {});
 });
