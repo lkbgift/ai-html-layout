a48f4069a6ab37685dd4fee4d17d465270d051ae
lkbgift <lkbgift@gmail.com>
2013-03-15 14:42:36 -0400

    removed slider and scripts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -37,5 +37,5 @@ require(["jquery",
   "foundation/foundation.topbar"
 ], function ($) {
   $(document).foundation('topbar');
-  $('#featured').foundation('orbit', { timer_speed: 5000, stack_on_small: false, animation_speed: 500, timer: false, bullets: true, }, function (response) {});
+ // $('#featured').foundation('orbit', { timer_speed: 5000, stack_on_small: false, animation_speed: 500, timer: false, bullets: true, }, function (response) {});
 });
