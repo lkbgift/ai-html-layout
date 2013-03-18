9f0ef7fafec66977e13997af3756b7abd2e2c5ce
lkbgift <lkbgift@gmail.com>
2013-03-18 18:21:33 -0400

    changes to logo size, rounded corners, and consistency in form roundness. Removed the extended page stats

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -39,5 +39,5 @@ require(["jquery",
   $(document).foundation('topbar');
   $('#brandslider').foundation('orbit', { timer_speed: 5000, stack_on_small: false, animation_speed: 750, timer: false, bullets: false, }, function (response) {});
   $('#case-sliders').foundation('orbit', {  stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {$('#case-sliders').find('.orbit-timer span').click();});
-  $('#featrap').foundation('orbit', {  stack_on_small: false, animation_speed: 750, bullets: false, bullets: true, }, function (response) {$('#featrap').find('.orbit-timer span').click();});
+
 });
